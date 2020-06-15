b6b42e3cc1459b494c3c17f9813f784ca03a7d94
Manh Cong <congnm.evergreen@gmail.com>
2020-06-15 17:24:03 +0700

    implement gitignore, vp, tablayout

--- app/src/main/java/g3/viewchoosephoto/PhotoPickerActivity.kt
+++ app/src/main/java/g3/viewchoosephoto/PhotoPickerActivity.kt
@@ -2,12 +2,28 @@ package g3.viewchoosephoto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_photo_picker.*
 
 class PhotoPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_picker)
 
+        setUpViewPagerWithTabLayout()
+        initViews()
+    }
+
+    private fun setUpViewPagerWithTabLayout() {
+
+
+        TabLayoutMediator(photo_picker_tab_layout_folder,photo_picker_view_pager) { tab, position ->
+            tab.text = "TAB $position"
+        }.attach()
+    }
+
+    private fun initViews() {
 
     }
+
 }
