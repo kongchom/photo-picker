9a3c636ee2fb7ecc7f3453a059f21e74d4ae2e38
Manh Cong <congnm.evergreen@gmail.com>
2020-06-16 17:35:01 +0700

    implement logic add chosen photos

--- app/src/main/java/g3/viewchoosephoto/PhotoPickerActivity.kt
+++ app/src/main/java/g3/viewchoosephoto/PhotoPickerActivity.kt
@@ -2,21 +2,42 @@ package g3.viewchoosephoto
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_photo_picker.*
 
 class PhotoPickerActivity : AppCompatActivity() {
 
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager: ViewPager2
     private var mAdapterPhoto: PhotoAdapter? = null
-    private var mLocalImages: ArrayList<LocalImage>? = null
-    private var mAlbumImages: ArrayList<AlbumImage>? = null
-    private var mPhotoChosen: ArrayList<LocalImage>? = null
+    private var mLocalImages: ArrayList<LocalImage>? = ArrayList()
+    private var mAlbumImages: ArrayList<AlbumImage>? = ArrayList()
+    private var mPhotoChoose: ArrayList<LocalImage> = ArrayList()
+    private var mFolderPosition: Int = 0
+    private lateinit var mCurrentFrag: PhotoViewerFragment
+
+    private var mLayoutManagerPhotoChoose: LinearLayoutManager? = null
+    private var mAdapterPhotoChoose: PhotoChooseAdapter? = null
+
+    private var onItemClick = object : ListenFromFragment{
+        override fun onItemClickInFragment(position: Int) {
+            Log.d("congnm lis", position.toString())
+            container_rv_chosen_image.visibility = View.VISIBLE
+            photo_picker_fab_next.visibility = View.VISIBLE
+            mPhotoChoose.add(mAlbumImages?.get(mFolderPosition)!!.localImages[position])
+            Log.d("congnm", mPhotoChoose.size.toString())
+            mLayoutManagerPhotoChoose?.scrollToPosition(mPhotoChoose.size - 1)
+            mAdapterPhotoChoose?.notifyDataSetChanged()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,25 +57,23 @@ class PhotoPickerActivity : AppCompatActivity() {
 
     private fun initData() {
         mAlbumImages = ImageUtils.getAllImage(applicationContext) as ArrayList<AlbumImage>?
-        mLocalImages?.addAll(mAlbumImages!![1]!!.localImages)
     }
 
     private fun setUpViewPagerWithTabLayout() {
         viewPager = findViewById(R.id.photo_picker_view_pager)
         tabLayout = findViewById(R.id.photo_picker_tab_layout_folder)
-        mAdapterPhoto = PhotoAdapter(applicationContext,mLocalImages)
-//        viewPager.adapter = FolderPhotoAdapter(mAlbumImages!!,applicationContext)
         viewPager.adapter = object: FragmentStateAdapter(this) {
             override fun getItemCount(): Int {
                 return mAlbumImages!!.size
             }
-
             override fun createFragment(position: Int): Fragment {
+                mFolderPosition = position
                 Log.d("congnm","onCreateFragmentPager")
-                return PhotoViewerFragment.newInstance(mAlbumImages!!, position)
+                mCurrentFrag = PhotoViewerFragment.newInstance(mAlbumImages!!, position)
+                mCurrentFrag.setListener(onItemClick)
+                return mCurrentFrag
             }
         }
-
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         TabLayoutMediator(tabLayout, viewPager,
             TabLayoutMediator.TabConfigurationStrategy { tab, position ->
@@ -63,6 +82,13 @@ class PhotoPickerActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        mAdapterPhotoChoose = PhotoChooseAdapter(applicationContext,mPhotoChoose)
+        mLayoutManagerPhotoChoose = LinearLayoutManager(applicationContext,LinearLayoutManager.HORIZONTAL, false)
+        photo_picker_rv_chosen_image.adapter = mAdapterPhotoChoose
+        photo_picker_rv_chosen_image.layoutManager = mLayoutManagerPhotoChoose
+    }
 
+    interface ListenFromFragment {
+        fun onItemClickInFragment(position: Int)
     }
 }
--- app/src/main/java/g3/viewchoosephoto/PhotoViewerFragment.kt
+++ app/src/main/java/g3/viewchoosephoto/PhotoViewerFragment.kt
@@ -7,39 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_folder_photo_viewer.*
 
 class PhotoViewerFragment: Fragment() {
 
     private var mLocalImages: ArrayList<LocalImage> = ArrayList()
-    private var mPhotoChoose: ArrayList<LocalImage> = ArrayList()
-    private var mAdapterPhotoChoose: PhotoChooseAdapter? = null
-    private var mLayoutManagerPhotoChoose: LinearLayoutManager? = null
-    private lateinit var rvChosenImg: View
-    private lateinit var floatingActionButton: FloatingActionButton
-    private lateinit var photoChooseRecyclerView: RecyclerView
+    private var onItemClick : PhotoPickerActivity.ListenFromFragment? = null
+
+    fun setListener(listener: PhotoPickerActivity.ListenFromFragment) {
+        onItemClick = listener
+    }
 
     private val onClickItemPhotoListener = PhotoAdapter.OnClickItemPhotoListener { position ->
         Log.d("congnm",mLocalImages[position].path)
-        rvChosenImg.visibility = View.VISIBLE
-        floatingActionButton.visibility = View.VISIBLE
-        if (mPhotoChoose.size < 60) {
-            mPhotoChoose.add(mLocalImages[position])
-            Log.d("congnm",mPhotoChoose.size.toString())
-            mLayoutManagerPhotoChoose?.scrollToPosition(mPhotoChoose.size - 1)
-            mAdapterPhotoChoose?.notifyDataSetChanged()
-//            if (mPhotoChoose.isEmpty()) {
-//                mTvPlease.setVisibility(View.VISIBLE)
-//                mRlContainerPlease.setVisibility(View.GONE)
-//            } else {
-//                mTvPlease.setVisibility(View.GONE)
-//                mRlContainerPlease.setVisibility(View.VISIBLE)
-//                mTvNumber.setText(getString(R.string.text_number, mPhotoChoose.size))
-//            }
-        }
+        onItemClick?.onItemClickInFragment(position)
     }
 
     override fun onCreateView(
@@ -53,13 +34,6 @@ class PhotoViewerFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d("congnm","onViewCreated")
         initDataRecyclerView()
-        rvChosenImg = activity?.findViewById(R.id.container_rv_chosen_image)!!
-        floatingActionButton = activity?.findViewById(R.id.photo_picker_fab_next)!!
-        photoChooseRecyclerView = activity?.findViewById(R.id.photo_picker_rv_chosen_image)!!
-        mAdapterPhotoChoose = PhotoChooseAdapter(context,mPhotoChoose)
-        photoChooseRecyclerView.adapter = mAdapterPhotoChoose
-        mLayoutManagerPhotoChoose = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
-        photoChooseRecyclerView.layoutManager = mLayoutManagerPhotoChoose
     }
 
     private fun initDataRecyclerView() {
